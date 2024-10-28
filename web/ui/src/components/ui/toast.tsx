3802191ec9363a29bce06183f68905300018509b
markfijneman <48481393+markfijneman@users.noreply.github.com>
2024-10-28 20:27:50 +0100

    Slightly adjusted theme colors

--- web/ui/src/components/ui/toast.tsx
+++ web/ui/src/components/ui/toast.tsx
@@ -27,7 +27,7 @@ const toastVariants = cva(
   {
     variants: {
       variant: {
-        default: "border bg-background text-foreground",
+        default: "border-secondary bg-secondary text-secondary-foreground",
         destructive:
           "destructive group border-destructive bg-destructive text-destructive-foreground",
       },
